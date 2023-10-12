9771c44966e1c7368f600273919949e7863f61ef
bhoomi1309 <143326623+bhoomi1309@users.noreply.github.com>
2023-10-12 13:14:50 +0530

    Update deleteduplicate.c

--- Array/deleteduplicate.c
+++ Array/deleteduplicate.c
@@ -1,26 +1,33 @@
 #include<stdio.h>
 void main(){
-	printf("Deleting Duplicate Elements");
-	int n,i,max=0;
+	printf("No. of Duplicate Elements");
+	int n,i,temp;
 	printf("\nEnter no. of Elements: ");
 	scanf("%d",&n);
 	int arr[n],j;
 	for(i=0;i<n;i++){
 		printf("Enter Number(%d): ",i+1);
 		scanf("%d",&arr[i]);
-		if(arr[i]>max){
-			max=arr[i];
-		}
 	}
-	int arr2[max];
-	for(i=0;i<=max;i++){
-		arr2[i]=0;
-	}
-	printf("Array without Duplicate Elements: ");
+	printf("Array: ");
 	for(i=0;i<n;i++){
-		if(arr2[arr[i]]==0){
-			printf("%d ",arr[i]);
+		printf("%d ",arr[i]);
+	}
+	for(i=0;i<n;i++){
+		for(j=i+1;j<n;j++){
+			if(arr[i]>arr[j]){
+			temp=arr[i];
+			arr[i]=arr[j];
+			arr[j]=temp;
+		    }
 		}
-		arr2[arr[i]]=1;
+	}
+	printf("\nArray without Duplicate Elements: ");
+	printf("%d ",arr[0]);
+	for(j=1;j<n;j++){
+		if(arr[j]!=arr[j-1]){
+			arr[i++]=arr[j];
+			printf("%d ",arr[j]);
+		}
 	}
 }
