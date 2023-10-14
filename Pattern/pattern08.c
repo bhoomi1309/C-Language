2c33ef343f848100d021a82e7be360c98b1b069b
bhoomi1309 <143326623+bhoomi1309@users.noreply.github.com>
2023-10-14 23:49:14 +0530

    Update pattern08.c

--- Pattern/pattern08.c
+++ Pattern/pattern08.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 void main(){
 	printf("Pattern\n");
-	int i,j,a;
+	int i,j,a,n;
+	printf("Enter no. of rows to print: ");
+	scanf("%d",&n);
 	printf("\n");
-	for(i=1;i<=5;i++){
+	for(i=1;i<=n;i++){
 		a=1;
-		for(j=1;j<=5-i;j++){
+		for(j=1;j<=n-i;j++){
 			printf(" ");
 		}
 		for(j=1;j<=2*i-1;j++){
