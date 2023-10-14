044bc55628bd4b55c4379a20fea8b5b8130db029
bhoomi1309 <143326623+bhoomi1309@users.noreply.github.com>
2023-10-14 23:50:44 +0530

    Update pattern11.c

--- Pattern/pattern11.c
+++ Pattern/pattern11.c
@@ -1,29 +1,31 @@
 #include<stdio.h>
 void main(){
 	printf("Pattern\n");
-	int i,j,a;
+	int i,j,a,n;
 	char ch='A';
+	printf("Enter no. of rows to print: ");
+	scanf("%d",&n);
 	printf("\n");
-	for(i=1;i<=5;i++){
+	for(i=1;i<=n;i++){
 		a=1;
-		for(j=1;j<=5-i;j++){
+		for(j=1;j<=n-i;j++){
 			printf(" ");
 		}
 		for(j=1;j<=2*i-1;j++){
 			if(j%2!=0){
 				if(i%2==0){
 					printf("%c",ch);
-		    	    ch++;
+		    	                ch++;
 				}
 				else{
 				    printf("%d",a);
-		    	    a++;	
+		    	            a++;	
 				}	
 			}
 			else{
 				printf(" ");
 			}
-		}
+	   	}
 	printf("\n");
-    }
+        }
 }
