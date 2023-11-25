e7d7b5e9c9ac528de3d70756f95df31bbbc3387d
bhoomi1309 <143326623+bhoomi1309@users.noreply.github.com>
2023-11-25 23:39:21 +0530

    Update swap.c

--- If Else/swap.c
+++ If Else/swap.c
@@ -1,4 +1,14 @@
 #include<stdio.h>
 void main(){
-	printf("Swapping 2 numbers")
+	printf("Swapping 2 Numbers");
+	int a,b;
+	printf("\nEnter any number: ");
+	scanf("%d",&a);
+	printf("Enter any number: ");
+	scanf("%d",&b);
+	printf("Before Swapping: \nNumber 1: %d \nNumber 2: %d",a,b);
+	int temp=a;
+	a=b;
+	b=temp;
+	printf("After Swapping: \nNumber 1: %d \nNumber 2: %d",a,b);
 }
